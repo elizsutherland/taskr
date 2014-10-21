8a6a2670877a01dfba271774377af1ae7765c9e2
Elizabeth Collins <escollins27@gmail.com>
2014-10-21 14:27:02 -0400

    Tasks can be completed,only list incomplete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.incomplete
     @task = Task.new
   end
 
@@ -9,7 +9,7 @@ class TasksController < ApplicationController
     if @task.save
       redirect_to root_path
     else
-      @tasks = current_user.tasks
+      @tasks = current_user.tasks.incomplete
       render :index
     end
   end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true
   validates :description, presence: true
+
+  def self.incomplete
+    where(completed: false)
+  end
 end
