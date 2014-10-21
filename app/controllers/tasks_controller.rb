6880b7408137ab2564dcb9bfba61fb8ab93ba584
Elizabeth Collins <escollins27@gmail.com>
2014-10-21 11:54:36 -0400

    Users can add titles and descriptions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,18 @@
 class TasksController < ApplicationController
   def index
     @tasks = current_user.tasks
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.new(task_params)
+    @task.save
+      redirect_to root_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
   end
 end
