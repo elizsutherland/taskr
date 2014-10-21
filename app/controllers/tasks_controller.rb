81b5316e28d99d8d467803b0835840cca38d1b2c
Elizabeth Collins <escollins27@gmail.com>
2014-10-21 14:43:02 -0400

    Add button to complete a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,10 +13,15 @@ class TasksController < ApplicationController
       render :index
     end
   end
+  def update
+    task = current_user.tasks.find(params[:id])
+    task.update(task_params)
+    redirect_to tasks_path
+  end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :description)
+    params.require(:task).permit(:title, :description, :completed)
   end
 end
